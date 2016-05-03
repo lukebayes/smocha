683cf7868cd3459ea40fcb683a0c978c715408f6
Luke Bayes <lukebayes@gmail.com>
2016-05-03 11:55:54 -0700

    Some evidence of end to end execution

--- src/smocha.js
+++ src/smocha.js
@@ -1,7 +1,34 @@
+require('/home/lukebayes/Projects/krypton/experimental/test/bootstrap.js');
+require('/home/lukebayes/Projects/krypton/experimental/modules/browser/kr-react/bootstrap.js');
+
+var FileRunner = require('./file_runner');
+var files = require('./files');
+var path = require('path');
 
 var Smocha = function(options) {
   this._options = options;
 };
 
+Smocha.prototype.run = function() {
+  console.log('running now!');
+  this._options.files && this._options.files.forEach(function(file) {
+    console.log('file:', file);
+    var runner = new FileRunner();
+    runner.runFile(path.resolve(file), function(err, status) {
+      if (err) {
+        console.log('ERRR:', err.stack);
+      }
+      console.log('FINISHED:', file);
+    });
+  });
+};
+
 module.exports = Smocha;
 
+
+if (require.main === module) {
+  files.scan('modules/common/kr-utils/test/*_test.js', function(err, result) {
+    var instance = new Smocha({files: result.paths});
+    instance.run();
+  });
+}
