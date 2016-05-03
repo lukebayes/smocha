e891e1ac2427349ec2ec84ad9a7894b89506eb70
Luke Bayes <lukebayes@gmail.com>
2016-05-02 17:12:10 -0700

    Removed file and process pool

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ module.exports = {
   ArrayIterator: require('./src/array_iterator'),
   Composable: require('./src/models/composable'),
   Events: require('./src/models/events'),
-  File: require('./src/models/file'),
-  ProcessPool: require('./src/models/process_pool'),
   Smocha: require('./src/smocha'),
   Suite: require('./src/models/suite'),
   Test: require('./src/models/test'),
