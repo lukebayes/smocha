e31c0c7dd4fee8390f21c98aacf4c94f53c429b1
Luke Bayes <lukebayes@gmail.com>
2017-08-24 00:18:16 -0400

    Added comments to Composite

--- src/composite.js
+++ src/composite.js
@@ -1,15 +1,24 @@
 
+/**
+ * Provide some basic infrastructure for entity composition.
+ */
 class Composite {
   constructor() {
     this.children = [];
     this.parent = null;
   }
 
+  /**
+   * Add a child to this entity and set that child's parent property.
+   */
   addChild(child) {
     child.parent = this;
     this.children.push(child);
   }
 
+  /**
+   * Enumerate each child.
+   */
   forEach(handler, index, array) {
     return this.children.forEach(handler, index, array);
   }
