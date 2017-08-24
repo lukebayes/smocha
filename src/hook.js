7fe8c7fd6646385c5539778b434f9e50eb4a9a90
Luke Bayes <lukebayes@gmail.com>
2017-08-24 00:17:11 -0400

    Updated comments for hook

--- src/hook.js
+++ src/hook.js
@@ -4,22 +4,29 @@ const Composite = require('./composite');
 function nullFunction() {};
 
 /**
- * Hooks are the base wrapper around all test declarations including before,
- * after, beforeEach, afterEach, it and describe.
+ * Hooks are the base wrapper around all test declarations including describe,
+ * before, after, beforeEach, afterEach and it.
  *
  * These declarations have subtly different syntax expectations, but in general
- * they can be synchronous or they declare themselves as asynchronous by either
- * declaring a callback argument in the handler or returning a Promise when the
- * handler is called.
+ * they can be synchronous or they can declare themselves as asynchronous by
+ * either declaring a callback argument in the handler or returning a Promise
+ * when the handler is called.
  */
 class Hook extends Composite {
   constructor(label, handler) {
     super();
     this._label = label;
-    this._handler = this._wrapHandler(handler);
+    this._handler = this._prepareHandler(handler);
   }
 
-  _wrapHandler(handler) {
+  /**
+   * Ensure we at least have a null function to call if the handler is
+   * undefined and otherwise prepare the provided handler by checking to see if
+   * it expects a callback argument, so that we can wrap it in a promise, or
+   * finally, ensure that we return whatever the implementation returns, which
+   * should either be a promise or undefined.
+   */
+  _prepareHandler(handler) {
     // this looks like a declaration that expects an async callback.
     if (handler && handler.length > 0) {
       function asyncHandler() {
@@ -41,10 +48,16 @@ class Hook extends Composite {
     return handler || nullFunction;
   }
 
+  /**
+   * Execute the provided handler.
+   */
   execute() {
     return this._handler();
   }
 
+  /**
+   * Get the full label (including parent labels) for this Hook.
+   */
   getLabel() {
     const base = this.parent ? this.parent.getLabel() + ' ' : '';
     return `${base}${this._label}`;
