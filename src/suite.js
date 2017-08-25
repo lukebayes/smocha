837bf59319bcf5e0819b648ec2dec7433878ca71
Luke Bayes <lukebayes@gmail.com>
2017-08-24 22:49:04 -0400

    Fixed suite hook set up

--- src/suite.js
+++ src/suite.js
@@ -19,6 +19,40 @@ class Suite extends Hook {
   }
 
   /**
+   * Get a list of beforeEach hooks that has the deepest declaration first and
+   * the nearest one last.
+   */
+  getBeforeEaches() {
+    let hooks = [];
+    let current = this;
+
+    while (current.parent) {
+      current = current.parent;
+      hooks = hooks.concat(current.beforeEaches);
+    }
+
+    hooks = hooks.concat(this.beforeEaches);
+
+    return hooks;
+  }
+
+  /**
+   * Get a list of afterEach hooks that has the nearest declaration first and
+   * the deepest one last.
+   */
+  getAfterEaches() {
+    let hooks = this.afterEaches;
+    let current = this;
+
+    while (current.parent) {
+      current = current.parent;
+      hooks = current.afterEaches.concat(hooks);
+    }
+
+    return hooks;
+  }
+
+  /**
    * Flatten the entire tree of tests and suites and insert all before/after
    * hooks around each test or suite as the case may be.
    *
@@ -31,9 +65,11 @@ class Suite extends Hook {
 
     // Create a hook set for beforeEaches, its and afterEaches
     this.tests.forEach((test) => {
-      result = result.concat(this.beforeEaches);
+      // NOTE(lbayes): All add all parent beforeEaches here
+      result = result.concat(this.getBeforeEaches());
       result.push(test);
-      result = result.concat(this.afterEaches);
+      result = result.concat(this.getAfterEaches());
+      // NOTE(lbayes): All add all parent afterEaches here
     });
 
     // Nest the children as result
