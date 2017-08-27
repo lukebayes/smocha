46bf035b04c5afdc6c8bc0f7856c72d9ccda791c
Luke Bayes <lukebayes@gmail.com>
2017-08-27 14:14:43 -0400

    Cleaned up hook visitor test

--- test/hook_visitor_test.js
+++ test/hook_visitor_test.js
@@ -1,9 +1,25 @@
-const HookVisitor = require('../').HookVisitor;
-const assert = require('chai').assert;
+const {Hook} = require('../');
+const {HookVisitor} = require('../');
+const {assert} = require('chai');
+
+class FakeDelegate {
+}
 
 describe('HookVisitor', () => {
+  let delegate;
+  let instance;
+
+  beforeEach(() => {
+    delegate = new FakeDelegate();
+    instance = new HookVisitor(delegate);
+  });
+
   it('is instantiable', () => {
-    const instance = new HookVisitor();
     assert(instance);
+  });
+
+  it('visits a test', () => {
+    const test = new Hook('abcd');
+    instance.visitTest(test);
   });
 });
