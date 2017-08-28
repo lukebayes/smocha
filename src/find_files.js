154bd1ab4b4edd79cd3a574f2c3dcc56ff7d05b0
Luke Bayes <lukebayes@gmail.com>
2017-08-28 15:54:39 -0400

    Introduced findFiles Will take an expression and a directory and return a list of files and stat objects sorted by modified time

--- src/find_files.js
+++ src/find_files.js
@@ -1,37 +1,32 @@
 const eachFileMatching = require('fileutils').eachFileMatching;
+const path = require('path');
 
 const DEFAULT_EXPRESSION = /.*_test.js/;
 const DEFAULT_DIRECTORY = 'test';
 
+/**
+ * Get all matching files and stats as an array of objects that are sorted with
+ * the most recently modified files first.
+ *
+ * Subsequent processes will read these files buffers and execute the tests.
+ */
 function findFiles(opt_expressionStr, opt_directory) {
   const expr = opt_expressionStr ? new RegExp(opt_expressionStr) : DEFAULT_EXPRESSION;
   const dir = opt_directory || DEFAULT_DIRECTORY;
 
   return new Promise((resolve, reject) => {
-    function completeHandler(err, files, stats) {
-      console.log('COMPLETE HANDER!!!!!!!!!!!!!!');
+    eachFileMatching(expr, dir, null, function completeHandler(err, files, stats) {
       if (err) return reject(err);
-
-      resolve({
-        files,
-        stats,
+      const results = files.map((file, index) => {
+        return {
+          file: path.resolve(file),
+          stat: stats[index],
+        };
+      }).sort((a, b) => {
+        return b.stat.mtimeMs - a.stat.mtimeMs;
       });
-    }
-
-    eachFileMatching(expr, dir, function(err, file, stat) {
-      console.log('>>>>>>>>>>> >YOOOOOOOO:', stat);
-      /*
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          files,
-          stats,
-          contents,
-        });
-      }
-      */
-    }, completeHandler);
+      resolve(results);
+    });
   });
 }
 
--- test/find_files_test.js
+++ test/find_files_test.js
@@ -3,11 +3,14 @@ const findFiles = require('../').findFiles;
 
 describe('findFiles', () => {
 
-  it.only('is callable', () => {
-    return findFiles('.*\.js', 'test/fixtures')
+  it('is callable', () => {
+    return findFiles('.*\.js', './test/fixtures')
       .then((files) => {
-        console.log('files.length:', files.files.length);
-        console.log('files:', files);
+        assert(files.length >= 2, 'Expected at least two files');
+
+        const firstStat = files[0].stat;
+        const secondStat = files[1].stat;
+        assert(firstStat.mtimeMs > secondStat.mtimeMs, 'Expected sort by modified time');
       });
   });
 });
